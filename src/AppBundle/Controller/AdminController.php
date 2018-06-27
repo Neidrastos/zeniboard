f523dea386d7083a1fbb973bf81ddaef756c950d
echauch <contact@kartenville.xyz>
2018-06-27 10:51:22 +0200

    test commit

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: echauchaix2018
+ * Date: 27/06/2018
+ * Time: 10:48
+ */
+
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
